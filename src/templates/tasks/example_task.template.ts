dbf82076b6431c2f024252d998d1620b9f951c46
Severin Ibarluzea <seveibar@gmail.com>
2023-01-18 16:44:42 -0800

    fix template

--- src/templates/tasks/example_task.template.ts
+++ src/templates/tasks/example_task.template.ts
@@ -1,8 +1,8 @@
 export default `
 
-import withTaskSpec from "../worker/with-task-spec"
+import { withTaskSpec } from "../worker/with-task-spec"
 
-export const example_task = createWithTaskSpec({
+export const example_task = withTaskSpec({
   task_name: "example_task",
   middlewares: [],
 } as const)
